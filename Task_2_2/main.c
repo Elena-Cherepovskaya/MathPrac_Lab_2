eb1355689a42baa1bfda50ff8817d908c71cbf3e
Elena-Cherepovskaya <145802662+Elena-Cherepovskaya@users.noreply.github.com>
2023-10-20 10:55:40 +0300

    Add files via upload

---
+++ Task_2_2/main.c
@@ -0,0 +1,37 @@
+//
+//  main.c
+//  2_2
+//
+//  Created by Лена on 18.10.2023.
+//
+
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+double _fast_pow (double n, int p)
+{
+    if (p == 0)
+        return  1.0;
+    
+    if ((p & 1) == 0)
+    {
+        double tmp_res = _fast_pow(n, p / 2);
+        return tmp_res * tmp_res;
+    }
+    else
+        return _fast_pow(n, p - 1) * n;
+}
+
+double fast_pow(double n, int p)
+{
+    return p >= 0 ? _fast_pow(n, p) : 1.0 / _fast_pow(n, -p);
+}
+
+int main(int argc, const char * argv[])
+{
+    printf("%lf\n", fast_pow(-3, 3));
+    
+    return 0;
+}
